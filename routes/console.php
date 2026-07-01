c8a1130ab83ac2e3ad12f3812c102535938b324f
sapyong13 <natunakantor@gmail.com>
2026-07-01 10:48:54 +0700

    Schedule SIPP sync hourly

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('sipp:sync-jadwal')->dailyAt('06:00')->withoutOverlapping();
+Schedule::command('sipp:sync-jadwal')->hourly()->withoutOverlapping();
